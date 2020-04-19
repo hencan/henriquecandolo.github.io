2b29340fe8b36ecbea6b79f2e3294dffdd33d5fc
hencan <49315755+hencan@users.noreply.github.com>
2020-04-19 12:58:57 -0300

    ver 0.531
    Nova versão do portifolio em react

---
+++ precache-manifest.d6e5e5ef299f7e280b914077169462d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a70cbaec7dcbc4ee67df4c08449fffa",
+    "url": "/projects/index.html"
+  },
+  {
+    "revision": "6ad7e81f40bc1efdb6d9",
+    "url": "/projects/static/css/main.7bf3169c.chunk.css"
+  },
+  {
+    "revision": "43c42cead55688c13bd5",
+    "url": "/projects/static/js/2.ebb3e927.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/projects/static/js/2.ebb3e927.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ad7e81f40bc1efdb6d9",
+    "url": "/projects/static/js/main.0b7ff341.chunk.js"
+  },
+  {
+    "revision": "86f18aaed0d9ab718f8f",
+    "url": "/projects/static/js/runtime-main.12c3baeb.js"
+  }
+]);
