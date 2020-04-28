262268a4609ebc220f304e8003a47bc9b45e83f6
hencan <49315755+hencan@users.noreply.github.com>
2020-04-27 22:03:41 -0300

    ver 0.533
    Correções no react

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ade841be2c500bcfc9c2a25e5f3be577.js"
+  "/portifolio/precache-manifest.ade841be2c500bcfc9c2a25e5f3be577.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portifolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
--- teste.js
+++
@@ -1,7 +0,0 @@
-var mainImg = document.querySelectorAll('.projectBoardImgMain');
-var thumbnailImg = document.querySelectorAll('.projectBoardImgThumbnailCell'); 
-
-function changeImage(x, y) {
-    var newImg = thumbnailImg[y].innerHTML.replace('projectBoardImgThumbnailCellClick', 'projectBoardImgMainClick')     
-    mainImg[x].innerHTML = newImg
-};
