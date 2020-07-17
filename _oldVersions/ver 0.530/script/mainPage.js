ad1be05ee30e5cc8d4022bd690bcc9ba6630ca5c
hencan <49315755+hencan@users.noreply.github.com>
2020-07-17 15:42:56 -0300

    update

--- _oldVersions/ver 0.530/script/mainPage.js
+++
@@ -1,8 +0,0 @@
-    document.querySelectorAll('a').forEach(link => {
-        link.onclick = function(e) {
-            e.preventDefault()
-            fetch(link.href)
-            .then(resp => resp.text())
-            .then(html => content.innerHTML = html)
-        }
-        }  )
